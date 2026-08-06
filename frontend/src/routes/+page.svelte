c31ac9e110d4d5da016c57ac75b951f33f2b3279
fenosoa <hugoandriamaromanana@gmail.com>
2026-08-06 11:33:13 +0300

    [test] card movement

---
+++ frontend/src/routes/+page.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+  import CardBoard from '$lib/CardBoard.svelte';
+</script>
+
+<CardBoard />
+
+<style>
+  :global(body) {
+    margin: 0;
+    padding: 0;
+    background: #0f0f13;
+  }
+</style>
