22634770ca5a0765d387cf6a47f814f90d356432
fenosoa <hugoandriamaromanana@gmail.com>
2026-08-07 18:31:06 +0300

    ui[fix] adapter + port change for dev

---
+++ ui/src/routes/+page.svelte
@@ -0,0 +1,10 @@
+<script>
+  import { goto } from '$app/navigation';
+  import { onMount } from 'svelte';
+  
+  onMount(() => {
+    goto('/login');
+  });
+</script>
+
+<p>Redirecting to login...</p>
